1df6e89c5b7f4fd2a643a4c79a4dca076487d04a
abigailridley <abigail.ridley@hotmail.co.uk>
2025-05-08 20:40:33 +0100

    add main image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
-import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container  mx-auto py-12">
-      <h1 className="text-4xl font-bodoni">Welcome to Our Wedding!</h1>
-      <Link href="/rsvp">
-        <button className=" font-dancing">RSVP</button>
-      </Link>
+    <div className="flex justify-center items-center min-h-screen bg-orange px-4">
+      <div className="relative w-[90vw] max-w-md aspect-square rounded-xl overflow-hidden">
+        <Image
+          src="/images/main.jpg"
+          alt="Gemma & Ali"
+          fill
+          className="object-cover grayscale brightness-90"
+          priority
+        />
+
+        {/* Overlay text in bottom right */}
+        <div className="absolute bottom-4 right-4 text-right font-bodoni text-4xl md:text-5xl tracking-tight leading-none text-white shadow-xl">
+          <div className="font-dancing">we're</div>
+          <div>getting</div>
+          <div>married</div>
+        </div>
+      </div>
     </div>
   );
 }
