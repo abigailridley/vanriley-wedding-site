3f82af72b0e99ab8c451d2d6b5685f03b50f4e98
abigailridley <abigail.ridley@hotmail.co.uk>
2025-05-08 07:57:39 +0100

    updating styling to header with pure tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,28 @@
-// components/header.tsx
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="py-4 bg-gray-100 shadow-md">
-      <nav className="max-w-6xl mx-auto flex justify-between items-center">
-        <Link
-          href="/"
-          className="text-lg font-semibold text-gray-700 hover:text-gray-900"
-        >
-          Home
-        </Link>
-        <div className="space-x-4">
-          <Link
-            href="/accom"
-            className="text-lg font-semibold text-gray-700 hover:text-gray-900"
-          >
-            Accommodation
+    <header className="py-4 bg-tan shadow-md text-dark-grey">
+      <nav className="container mx-auto flex justify-between items-center">
+        {/* Left column: links */}
+        <div className="space-x-4 font-playfair text-lg">
+          <Link href="/" className="hover:underline">
+            home
           </Link>
-          <Link
-            href="/rsvp"
-            className="text-lg font-semibold text-gray-700 hover:text-gray-900"
-          >
+          <span>|</span>
+          <Link href="/accom" className="hover:underline">
+            accommodation
+          </Link>
+          <span>|</span>
+          <Link href="/rsvp" className="hover:underline">
             RSVP
           </Link>
         </div>
+
+        <div className="font-playfair text-2xl tracking-wide">GEMMA & ALI</div>
+
+        {/* Right column: empty for now */}
+        <div className="w-24" />
       </nav>
     </header>
   );
