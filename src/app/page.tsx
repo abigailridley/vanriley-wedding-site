dec0a010ad1faeff07ae36a22ece64b96d54397d
abigailridley <abigail.ridley@hotmail.co.uk>
2025-05-08 07:57:24 +0100

    removing shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container mx-auto py-12">
-      <h1 className="text-4xl font-semibold">Welcome to Our Wedding!</h1>
+    <div className="container  mx-auto py-12">
+      <h1 className="text-4xl font-bodoni">Welcome to Our Wedding!</h1>
       <Link href="/rsvp">
-        <Button>RSVP</Button>
+        <button className=" font-dancing">RSVP</button>
       </Link>
     </div>
   );
