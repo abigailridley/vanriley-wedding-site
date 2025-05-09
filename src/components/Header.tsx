eb242e98ac2b17e8006fffabd3da121deab496d5
abigailridley <abigail.ridley@hotmail.co.uk>
2025-05-09 15:30:13 +0100

    responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="py-4 bg-tan shadow-md text-dark-grey relative">
       <nav className="container mx-auto flex justify-between items-center relative">
         {/* Left column: links */}
-        <div className="hidden sm:flex space-x-4 font-playfair text-lg tracking-widest">
+        <div className="hidden md:flex space-x-4 font-playfair text-md tracking-widest">
           <Link href="/" className="hover:underline">
             home
           </Link>
@@ -20,7 +20,7 @@ const Header = () => {
         </div>
 
         {/* Centre column: GEMMA & ALI - always centered */}
-        <div className="absolute left-1/2 transform -translate-x-1/2 sm:static sm:transform-none font-playfair text-2xl tracking-tighter text-center pt-5 sm:pt-0">
+        <div className="absolute left-1/2 transform -translate-x-1/2 md:static sm:transform-none font-playfair text-2xl tracking-tighter text-center pt-5 sm:pt-0">
           GEMMA & ALI
         </div>
 
@@ -29,7 +29,7 @@ const Header = () => {
       </nav>
 
       {/* Links shown below on small screens */}
-      <div className="sm:hidden flex justify-center space-x-4 mt-4 font-playfair text-lg tracking-widest pt-5 sm:pt-0">
+      <div className="md:hidden flex justify-center space-x-4 mt-4 font-playfair text-md tracking-wide  pt-5 sm:pt-0">
         <Link href="/" className="hover:underline">
           home
         </Link>
