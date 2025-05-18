c69fec66471ee1456afbd039c8a13456d6ef613a
abigailridley <abigail.ridley@hotmail.co.uk>
2025-05-18 11:39:30 +0100

    Update page.tsx

--- src/app/rsvp/page.tsx
+++ src/app/rsvp/page.tsx
@@ -124,8 +124,8 @@ const RSVPForm = () => {
         </p>
         <hr className="my-4 w-2/3 mx-auto border-green opacity-50" />
         <p>
-          We are unable to accommodate children or plus ones — thank you for
-          understanding.
+          Guests are limited to inite-only, we are unable to accommodate
+          children or plus ones — thank you for understanding.
         </p>
         <hr className="my-4 w-2/3 mx-auto border-green opacity-50" />
         <p>
